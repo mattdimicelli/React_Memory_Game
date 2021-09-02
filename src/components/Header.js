efc823628d5634292d877ce8954780ab699cb232
Matt Di Micelli <matthewdimicelli@gmail.com>
2021-09-02 17:45:39 -0400

    fix grammatical error in instructions

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(props) {
             <div className='instructions'>
                 <h1>Flag Memory Game</h1>
                 <p>Get points by clicking on any flag that you haven't yet clicked on! 
-                Your score resets if you forget and click and flag a second time! </p>
+                Your score resets if you forget and click a flag a second time! </p>
             </div> 
             <div className='scoreboard-and-status'>
                 <div className='scoreboard'>
